887a17d52cc6296fd9cf06701705e822ad60eeb9
ashkuc <ashkucc@gmail.com>
2024-01-31 11:54:28 +0100

    fixes

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,32 +1,32 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import {HardhatUserConfig} from 'hardhat/config'
+import '@nomicfoundation/hardhat-toolbox'
 import '@nomicfoundation/hardhat-ethers'
-import { config as dotenvConfig } from "dotenv";
+import {config as dotenvConfig} from 'dotenv'
 import {HttpNetworkUserConfig} from 'hardhat/types'
 
 dotenvConfig()
 
-const accounts = [process.env.PRIVATE_KEY!];
+const accounts = [process.env.PRIVATE_KEY!]
 
 export const uniqsu: HttpNetworkUserConfig = {
-  url: 'https://rpc-o.unq.uniq.su',
+  url: 'https://rpc.unq.uniq.su',
   chainId: 8882,
   accounts,
-};
+}
 
 export const opal: HttpNetworkUserConfig = {
   url: 'https://rpc-opal.unique.network',
   chainId: 8882,
   accounts,
-};
+}
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.19",
-  defaultNetwork: "hardhat",
+  solidity: '0.8.19',
+  defaultNetwork: 'hardhat',
   networks: {
     hardhat: {},
     opal, uniqsu,
   },
-};
+}
 
-export default config;
+export default config
--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,41 +1,50 @@
-import { ethers } from "hardhat";
+import {ethers} from 'hardhat'
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const currentTimestampInSeconds = Math.round(Date.now() / 1000)
+  const unlockTime = currentTimestampInSeconds + 60
 
-  const signer = await ethers.provider.getSigner(0);
-  const signerAddress = await signer.getAddress();
-  const signerBalance = await ethers.provider.getBalance(signerAddress);
+  const signer = await ethers.provider.getSigner(0)
+  const signerAddress = await signer.getAddress()
+  const signerBalance = await ethers.provider.getBalance(signerAddress)
 
-  console.log(`Deploying Lock. Owner: ${signerAddress}. Balance: ${signerBalance.toString()}`);
+  console.log(`Deploying Lock. Owner: ${signerAddress}. Balance: ${signerBalance.toString()}`)
 
-  const lockedAmount = 100n;
+  const lockedAmount = 100n
 
-  const lockFactory = await ethers.getContractFactory("Lock");
+  const lockFactory = await ethers.getContractFactory('Lock')
+
+  const tx = await lockFactory.getDeployTransaction(unlockTime, {
+    customData: [unlockTime],
+    value: lockedAmount,
+  })
+
+  // throws here
+  const estimatedGas = await signer.estimateGas(tx)
+
+  console.log(`Estimated gas: ${estimatedGas.toString()}`)
 
   const lock = await lockFactory.deploy(unlockTime, {
     customData: [unlockTime],
     value: lockedAmount,
-  });
+    gasLimit: 3000000,
+  })
 
-  await lock.waitForDeployment();
-  const lockAddress = await lock.getAddress();
+  await lock.waitForDeployment()
+  const lockAddress = await lock.getAddress()
 
   console.log(
-    `Lock with ${lockedAmount.toString()} and unlock timestamp ${unlockTime} deployed to ${lockAddress}`
-  );
+    `Lock with ${lockedAmount.toString()} and unlock timestamp ${unlockTime} deployed to ${lockAddress}`,
+  )
 
-  const lockBalance = await ethers.provider.getBalance(lockAddress);
-  console.log(`Lock balance: ${lockBalance.toString()}`);
+  const lockBalance = await ethers.provider.getBalance(lockAddress)
+  console.log(`Lock balance: ${lockBalance.toString()}`)
 
-  const lockUnlockTime = await lock.unlockTime();
-  console.log(`Lock unlock time: ${lockUnlockTime.toString()}`);
+  const lockUnlockTime = await lock.unlockTime()
+  console.log(`Lock unlock time: ${lockUnlockTime.toString()}`)
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+  console.error(error)
+  process.exitCode = 1
+})
